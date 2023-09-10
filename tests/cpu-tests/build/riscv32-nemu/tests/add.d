5401edadc2c8af9846d6f543c6e500f0de58fc45
22050335-Zhang Shoufeng <zhangshoufeng2077@outlook.com>
2023-09-10 22:20:59 +0800

    init am-kernels

---
+++ tests/cpu-tests/build/riscv32-nemu/tests/add.d
@@ -0,0 +1,8 @@
+/home/shawnzhang/ysyx-workbench/am-kernels/tests/cpu-tests/build/riscv32-nemu/tests/add.o: \
+ /home/shawnzhang/ysyx-workbench/am-kernels/tests/cpu-tests/tests/add.c \
+ /home/shawnzhang/ysyx-workbench/am-kernels/tests/cpu-tests/include/trap.h \
+ /home/shawnzhang/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/shawnzhang/ysyx-workbench/abstract-machine/am/include/arch/riscv32-nemu.h \
+ /home/shawnzhang/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/shawnzhang/ysyx-workbench/abstract-machine/klib/include/klib.h \
+ /home/shawnzhang/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
